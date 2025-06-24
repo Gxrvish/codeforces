0d324478f1039d5b202113f02215e1bea5c59c6a
Gxrvish <garvishpanchal004@gmail.com>
2025-06-24 22:23:39 +0530

    update

---
+++ DontTrytoCount.java
@@ -0,0 +1,225 @@
+import java.io.*;
+import java.util.*;
+
+public class DontTrytoCount {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    // Debug Function
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    // GCD
+    static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    // LCM
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    // Prime Check
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    // Sieve of Eratosthenes
+    static boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+
+    // Modular Exponentiation
+    static long modExp(long base, long exp, long mod) {
+        long result = 1;
+        base = base % mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    // Factorial Modulo
+    static long factorialMod(int n, int mod) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = (result * i) % mod;
+        }
+        return result;
+    }
+
+    // Custom Comparator for Sorting
+    static class Pair implements Comparable<Pair> {
+        int first, second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return Integer.compare(this.first, o.first); // Ascending order by first
+        }
+    }
+
+    // Prefix Sum
+    static int[] prefixSum(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        return prefix;
+    }
+
+    // Binary Search
+    static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return mid;
+            if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1; // Not found
+    }
+
+    static boolean check(String s, String x) {
+        return s.contains(x);
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = in.nextInt();
+            while (testCases-- > 0) {
+                int n = in.nextInt();
+                int m = in.nextInt();
+                String x = in.next();
+                String s = in.next();
+
+                String x0 = x;
+                String x1 = x0 + x0;
+                String x2 = x1 + x1;
+                String x3 = x2 + x2;
+                String x4 = x3 + x3;
+                String x5 = x4 + x4;
+
+                int ans = -1;
+                if (x0.contains(s)) {
+                    ans = 0;
+                } else if (x1.contains(s)) {
+                    ans = 1;
+                } else if (x2.contains(s)) {
+                    ans = 2;
+                } else if (x3.contains(s)) {
+                    ans = 3;
+                } else if (x4.contains(s)) {
+                    ans = 4;
+                } else if (x5.contains(s)) {
+                    ans = 5;
+                }
+
+                out.println(ans);
+            }
+            out.close();
+        } catch (
+
+        Exception e) {
+            return;
+        }
+    }
+}
