d232cedfcea49a89b343878f4739e978c54c4024
Gxrvish <garvishpanchal004@gmail.com>
2026-01-04 11:59:59 +0530

    update

---
+++ GrasshopperOnALine.java
@@ -0,0 +1,100 @@
+
+import java.io.*;
+import java.util.*;
+
+public class GrasshopperOnALine {
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    // Debug Function
+    static void debug(Object... o) {
+        System.err.println(Arrays.deepToString(o));
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases = in.nextInt();
+            while (testCases-- > 0) {
+                // write code here
+
+                int x = in.nextInt();
+                int k = in.nextInt();
+                if (x % k != 0) {
+                    out.println(1);
+                    out.println(x);
+                } else {
+                    out.println(2);
+                    out.println((x - 1) + " 1");
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
